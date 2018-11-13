fe2bc2547db85f64dbabd685081cf4280b5d75e9
Furaxus <jeremy.zaragoza@gmail.com>
2018-11-13 15:30:00 +0100

    tp 13/11/18

--- registres.0.vhd
+++ registres.0.vhd
@@ -19,15 +19,13 @@ use IEEE.std_logic_unsigned.all;
 entity registres is
 
 	-- definition des parametres generiques
---	generic	(
+	generic	(
 		-- largeur du bus de donnees par defaut
---		DBUS_WIDTH	: integer := 32; -- registre de 32 bits par defaut
+		DBUS_WIDTH	: integer := 32; -- registre de 32 bits par defaut
 
 		-- largeur du bus adr pour acces registre soit 32 (2**5) par defaut
---		ABUS_WIDTH	: integer := 5;
-
-		-- definition du front actif d'ecriture par defaut
---		ACTIVE_FRONT: std_logic := '1' );
+		ABUS_WIDTH	: integer := 5
+	);
 
 	-- definition des entrees/sorties
 	port 	(
@@ -36,13 +34,11 @@ entity registres is
 		________
 
 		-- bus d'adresse et donnees
-		________
-		________
-		________
+		ADR_A, ADR_B, ADR_W : in std_logic_vector (ABUS_WIDTH downto 0);
 
 		-- Ports de sortie
-		________
-		________
+		QA, QB : out is array (0 to ABUS_WIDTH-1) of std_logic_vector (DBUS_WIDTH-1 downto 0)
+	);
 
 end registres;
 
@@ -55,7 +51,7 @@ end registres;
 architecture behavior of registres is
 
 	-- definitions de types (index type default is integer)
-	type FILE_REGS is array (0 to ______) of std_logic_vector (______ downto 0);
+	type FILE_REGS is array (0 to (2**3)-1) of std_logic_vector (31 downto 0);
 
 	-- definition des ressources internes
 	signal REGS : FILE_REGS; -- le banc de registres
@@ -64,24 +60,26 @@ begin
 
 ---------------------------------
 -- affectation des bus en lecture
-________
-________
-________
-________
+-- DOMAINE COMBINATOIRE
+	QA <= REGS(conv_integer(ADR_A)) when ADR_A /= conv_std_logic_vector(0, ADR_A'length)
+		else (others => '0');
+	QB <= REGS(conv_integer(ADR_B)) when ADR_B /= conv_std_logic_vector(0, ADR_B'length)
+		else (others => '0');
 
 -----------------
 -- Process P_REGS
-P_REGS: process(________)
+P_REGS: process(RST, CLK)
 begin
 	-- test du reset
 	if RST='0' then
-		 REGS <= (others => ______);
-	-- test front actif d'horloge
-	elsif (CLK'event and CLK=____) then
-		-- test si ecriture dans le registre
-		if ((W='0') and ADR_W /= ________ then
-			REGS(______) <= D;
-		end if;
+		REGS <= (others => conv_std_logic_vector(0,DBUS_WIDTH));
+		-- ON NE RESET PAS LES SORTIES CAR LES PROCESS AU DESSUS LES MODIFIES !!!!!
+		-- LES VALEURS SERONT MAINTENU DE FORCE QUAND LA CONDITION N'EST PAS RESPECTEE
+		-- QA <= (others => '0');
+		-- QB <= (others => '0');
+	-- test front actif d'horloge et si ecriture dans le registre
+	elsif (rising_edge(CLK) and W='0' and ADR_W /= ABUS_WIDTH) then
+		REGS(conv_integer(ADR_W)) <= D;
 	end if;
 end process P_REGS;
 
